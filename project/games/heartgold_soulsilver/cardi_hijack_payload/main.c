1991deb9d60b3587325f8ed3c4b8f4cc1d801dad
DevreeseJorik <jorik.devreese@outlook.com>
2025-12-21 18:18:15 +0100

    project: heartgold_soulsilver: fix CARDi_ReadCard custom overlays
    The previous hijack only hijacked one of the two possible return statements in the CARDi_ReadCard function.
    You could just also add a second hook in the other return statement, however, CARDi_ReadCard is actually
    never called directly. Instead, there is a function that returns the address of this function, which
    is then used to call it. We can edit the immediate containing the address of CARDi_ReadCard to our custom
    payload instead, and then make sure to call CARDi_ReadCard ourselves in our payload.
    
    This makes things a little cleaner and lets us drop the compiling of the hook seperately.
    
    Signed-off-by: DevreeseJorik <jorik.devreese@outlook.com>

--- project/games/heartgold_soulsilver/cardi_hijack_payload/main.c
+++ project/games/heartgold_soulsilver/cardi_hijack_payload/main.c
@@ -1,9 +1,34 @@
-#include "graphics/poke_sprite.h"
+#include "util/functions.h"
+#include "util/memory.h"
+
+void write_if_matching8(u8 *address, u32 match, u8 value) {
+  if (read_u32_unaligned((u32 *)address) == match) {
+    write_u8(address, value);
+  }
+}
+
+void write_if_matching16(u16 *address, u32 match, u16 value) {
+  if (read_u32_unaligned((u32 *)address) == match) {
+    write_u16(address, value);
+  }
+}
+
+void write_if_matching32(u32 *address, u32 match, u32 value) {
+  if (read_u32_unaligned((u32 *)address) == match) {
+    write_u32(address, value);
+  }
+}
 
 __attribute__((naked)) __attribute__((section(".text.main")))
 __attribute__((target("arm"))) void
 main(void) {
-  __asm__ volatile(".word 0x08bd8070\n");
+  __asm__ volatile("push {r0-r7, lr}\n");
+  CARDi_ReadCard(); // original function call
 
-  // compiler doesn't recognise ldmiaeq sp!,{r4,r5,r6,pc} => 70  80  bd  08
+  // custom overlay modifications:
+
+  // disable sprite pop up in battle
+  write_if_matching8((u8 *)0x0221fde4, 0xDA002804, 0x0);
+
+  __asm__ volatile("pop {r0-r7, pc}\n");
 }
--- project/games/heartgold_soulsilver/uncompressor_hijack_payload/main.c
+++ project/games/heartgold_soulsilver/uncompressor_hijack_payload/main.c
@@ -5,6 +5,7 @@ __attribute__((target("arm"))) void
 main(void) {
   // __asm__ volatile("bx lr\n");
   __asm__ volatile("push {r0-r7, lr}\n");
-  write_u32((u32 *)0x020dd638, 0xea0b9ab0); // cardi_hijack_hook => 0x23C4100
+  write_u32((u32 *)0x0020dd8c8, 0x23C4100);
+  set_sprite_hooks();
   __asm__ volatile("pop {r0-r7, pc}\n");
 }
