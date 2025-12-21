1f6462390fdaaf08f67adbd747ecdb098d3c7bd8
DevreeseJorik <jorik.devreese@outlook.com>
2025-12-21 10:42:43 +0100

    project: installer.lua: add installer for heartgold_soulsilver
    Writes the packed box data to the ingame box data, and forces this data to be saved by
    editing the function responsible for checking which boxes were edited to always return true
    for the entire set of box flags. Also installs the initial payload to gift data.
    
    Signed-off-by: DevreeseJorik <jorik.devreese@outlook.com>

---
+++ project/installer.lua
@@ -0,0 +1,50 @@
+local giftDataOffset = 0x9E4C
+local boxDataOffset = 0xF710
+
+local basePointer = mainmemory.read_u32_le(0x021D2228 - 0x2000000)
+local giftDataPointer = basePointer + giftDataOffset
+local boxDataPointer = basePointer + boxDataOffset
+
+local config = {
+    giftDataFile = "out/heartgold_soulsilver/initial_payload/bin/initial_payload.bin",
+    boxDataFile = "packager/packed.bin"
+}
+
+local function writeBinToMemory(filePath, memoryAddress)
+    local file = io.open(filePath, "rb")
+    if not file then
+        error("Could not open file: " .. filePath)
+    end
+
+    local data = file:read("*all")
+    file:close()
+
+    mainmemory.write_bytes_as_array(memoryAddress - 0x2000000, {string.byte(data, 1, #data)})
+    print(string.format("Wrote %d bytes to 0x%X from %s", #data, memoryAddress, filePath))
+end
+
+local function writeGiftHeader(memoryAddress)
+    local giftHeader = {
+        0x06, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0xF6, 0x12, 0x02,
+        0x00, 0x00, 0x07, 0x00, 0x19, 0x82, 0x04, 0x02, 0x47, 0x03, 0x03, 0x00, 0x00
+    }
+    mainmemory.write_bytes_as_array(memoryAddress - 0x2000000, giftHeader)
+end
+
+local function forceBoxSave() 
+    local boxSaveBitsPointer = 0x02028c8e
+    mainmemory.write_u8(boxSaveBitsPointer - 0x2000000, 0xFF)
+end
+
+local function install()
+    print(string.format("Base Pointer: 0x%X", basePointer))
+
+    writeGiftHeader(giftDataPointer)
+    writeBinToMemory(config.giftDataFile, giftDataPointer + 0x18)
+    writeBinToMemory(config.boxDataFile, boxDataPointer)
+    forceBoxSave()
+end
+
+print("Starting installation...")
+install()
+print("Installation complete.")
