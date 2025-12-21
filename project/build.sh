613e5b186e686ad7adec4cf518e522eb64fec108
DevreeseJorik <jorik.devreese@outlook.com>
2025-12-21 18:23:50 +0100

    project: build.sh: completely build packed.bin for installer
    This outputs the complete packed.bin from scratch, which can be installed in a single click
    using the installer.lua ingame.
    
    Signed-off-by: DevreeseJorik <jorik.devreese@outlook.com>

--- project/build.sh
+++ project/build.sh
@@ -3,3 +3,11 @@ mkdir build
 cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake ../
 make
+
+cd ..
+
+python3 ../assets/image_converter.py -input_path=images_in -output_path=images_out
+
+python3 packager.py 
+./dump.sh packager/packed.bin > packager/packed_dump.txt
+./dump.sh packager/unpacked.bin > packager/unpacked_dump.txt
