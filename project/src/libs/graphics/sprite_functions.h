ee39997e5ec944e6ab7d59a10e901ae2ac0aad28
DevreeseJorik <jorik.devreese@outlook.com>
2025-12-26 20:17:43 +0100

    various: small cleanup and rename functions to match lib
    Signed-off-by: DevreeseJorik <jorik.devreese@outlook.com>

--- project/src/libs/graphics/sprite_functions.h
+++ project/src/libs/graphics/sprite_functions.h
@@ -3,11 +3,12 @@
 typedef void NarcSpriteData;
 typedef void NarcPaletteData;
 
-#define fp_tryLoadSpriteFromNarc                                               \
+#define fp_NNS_G2dGetUnpackedCharacterData                                     \
   fp_arm(0x020b70a8, BOOL, (void *, NarcSpriteData *))
 
-static inline BOOL tryLoadSpriteFromNarc(void *dest, NarcSpriteData *src) {
-  return fp_tryLoadSpriteFromNarc(dest, src);
+static inline BOOL NNS_G2dGetUnpackedCharacterData(void *dest,
+                                                   NarcSpriteData *src) {
+  return fp_NNS_G2dGetUnpackedCharacterData(dest, src);
 }
 
 #define fp_decodeSprite fp_thumb(0x02009d28, void, (void *, uint32_t))
@@ -15,8 +16,9 @@ static inline void decodeSprite(void *spriteBuffer, uint32_t gameId) {
   fp_decodeSprite(spriteBuffer, gameId);
 }
 
-#define fp_tryLoadPaletteFromNarc                                              \
+#define fp_NNS_G2dGetUnpackedPaletteData                                       \
   fp_arm(0x020b7140, BOOL, (void *, NarcPaletteData *))
-static inline BOOL tryLoadPaletteFromNarc(void *dest, NarcPaletteData *src) {
-  return fp_tryLoadPaletteFromNarc(dest, src);
+static inline BOOL NNS_G2dGetUnpackedPaletteData(void *dest,
+                                                 NarcPaletteData *src) {
+  return fp_NNS_G2dGetUnpackedPaletteData(dest, src);
 }
