3fcff563baa78b820527033a38df521aac8ebdac
DevreeseJorik <jorik.devreese@outlook.com>
2025-12-26 20:16:33 +0100

    archive_data_load: load custom teams based on trainer id
    Signed-off-by: DevreeseJorik <jorik.devreese@outlook.com>

--- project/src/payloads/uncompressor/main.c
+++ project/src/payloads/uncompressor/main.c
@@ -4,7 +4,8 @@ __attribute__((naked)) __attribute__((section(".text.main")))
 __attribute__((target("arm"))) void
 main(void) {
   __asm__ volatile("push {r0-r7, lr}\n");
-  write_u32((u32 *)0x0020dd8c8, 0x23C4100);
+  write_u32((u32 *)0x020dd8c8, 0x023C4100);
   set_sprite_hooks();
+  write_u32((u32 *)0x0200750a, 0xff79f3bc);
   __asm__ volatile("pop {r0-r7, pc}\n");
 }
