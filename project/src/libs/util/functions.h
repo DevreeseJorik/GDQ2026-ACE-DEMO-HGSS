f4681c6d2916fbc222938b1d4ec750b06b723fd8
DevreeseJorik <jorik.devreese@outlook.com>
2025-12-27 11:28:31 +0100

    archive_data_load_malloc: replace narc_sprite_load hack with proper implementation
    commit f727d9c8567ff84ea433585b6bae1f5ff10be73f introduced a hack to make the first sprite on
    trainer battle load. However, the actual root issue is still there, which is that the pointer we use
    points to the start of archive data for all pokémon in battle, rather than the one currently being loaded.
    
    This causes more issues later in battle, so rather than having this dirty hack, implement a proper fix by
    getting the archive id through archive_data_load_malloc's call.
    
    You may notice a small 'hack' where we only set the archive info if it matches one that we expect, and
    clear this data upon loading palette.
    
    This is needed as our logic wants to have each custom pokémon have a unique palette, but there are multiple
    pokémon with the same palette ingame, and these are also loaded through archive_data_load_malloc. Therefore,
    if we would check on palette archive info, we'd be affecting other pokémon. So we only update the archive
    data for pokémon sprite archive info, which we then also use for our palette.
    
    Finally, ensure to clear this info after loading palette, otherwise the next pokémon will get the previous
    custom pokémon's sprite.
    
    Signed-off-by: DevreeseJorik <jorik.devreese@outlook.com>

--- project/src/libs/util/functions.h
+++ project/src/libs/util/functions.h
@@ -1,7 +1,11 @@
 #ifndef _FUNCTIONS_H
 #define _FUNCTIONS_H
 
+#include "types.h"
 #include <stdint.h>
+
+typedef void NarcSpriteData;
+typedef void NarcPaletteData;
 
 #define fp_arm(address, type, param)                                           \
   ((type(*) param)((uintptr_t)(address) & ~1))
@@ -12,11 +16,43 @@
 #define fp_cardi_readcard fp_arm(0x020dd570, void, (void))
 static inline void CARDi_ReadCard(void) { fp_cardi_readcard(); }
 
-#define fp_archive_data_load_index                                             \
+#define fp_ArchiveDataLoadIndex                                                \
   fp_thumb(0x0200732c, void, (void *, const char *, int, int, int))
 static inline void ArchiveDataLoadIndex(void *data, const char *name, int index,
                                         int ofset, int size) {
-  fp_archive_data_load_index(data, name, index, ofset, size);
+  fp_ArchiveDataLoadIndex(data, name, index, ofset, size);
+}
+
+#define fp_ArchiveDataLoadIndexMalloc                                          \
+  fp_thumb(0x0200740c, void *,                                                 \
+           (const char *name, int index, int heap_id, int ofs, int ofs_size,   \
+            int flag))
+
+static void *ArchiveDataLoadIndexMalloc(const char *name, int index,
+                                        int heap_id, int ofs, int ofs_size,
+                                        int flag) {
+  return fp_ArchiveDataLoadIndexMalloc(name, index, heap_id, ofs, ofs_size,
+                                       flag);
+}
+
+#define fp_NNS_G2dGetUnpackedCharacterData                                     \
+  fp_arm(0x020b70a8, BOOL, (void *, NarcSpriteData *))
+
+static inline BOOL NNS_G2dGetUnpackedCharacterData(void *dest,
+                                                   NarcSpriteData *src) {
+  return fp_NNS_G2dGetUnpackedCharacterData(dest, src);
+}
+
+#define fp_decodeSprite fp_thumb(0x02009d28, void, (void *, uint32_t))
+static inline void decodeSprite(void *spriteBuffer, uint32_t gameId) {
+  fp_decodeSprite(spriteBuffer, gameId);
+}
+
+#define fp_NNS_G2dGetUnpackedPaletteData                                       \
+  fp_arm(0x020b7140, BOOL, (void *, NarcPaletteData *))
+static inline BOOL NNS_G2dGetUnpackedPaletteData(void *dest,
+                                                 NarcPaletteData *src) {
+  return fp_NNS_G2dGetUnpackedPaletteData(dest, src);
 }
 
 #endif // _FUNCTIONS_H
