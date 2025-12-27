6dc084324f2095775e588d3a40ca19e2bf7b3402
DevreeseJorik <jorik.devreese@outlook.com>
2025-12-27 11:28:31 +0100

    archive_data_load_malloc: replace narc_sprite_load hack with proper implementation
    commit f727d9c8567ff84ea433585b6bae1f5ff10be73f introduced a hack to make the first sprite on
    trainer battle load. However, the actual root issue is still there, which is that the pointer we use
    points to the start of archive data for all pokémon in battle, rather than the one currently being loaded.
    
    This causes more issues later in battle, so rather than having this dirty hack, implement a proper fix by
    getting the archive id through archive_data_load_malloc's call.
    
    You may notice a small 'hack' where we only set the archive info if it matches one that we expect, and
    clear this data upon loading palette.
    
    This is needed as our logic wants to have each custom pokémon have a unique palette, but there are multiple
    pokémon with the same palette ingame, and these are also loaded through archive_data_load_malloc. Therefore,
    if we would check on palette archive info, we'd be affecting other pokémon. So we only update the archive
    data for pokémon sprite archive info, which we then also use for our palette.
    
    Finally, ensure to clear this info after loading palette, otherwise the next pokémon will get the previous
    custom pokémon's sprite.
    
    Signed-off-by: DevreeseJorik <jorik.devreese@outlook.com>

--- project/src/payloads/uncompressor/main.c
+++ project/src/payloads/uncompressor/main.c
@@ -4,8 +4,23 @@ __attribute__((naked)) __attribute__((section(".text.main")))
 __attribute__((target("arm"))) void
 main(void) {
   __asm__ volatile("push {r0-r7, lr}\n");
+  // cardi_readcard: overwrites immediate with custom pointer
   write_u32((u32 *)0x020dd8c8, 0x023C4100);
-  set_sprite_hooks();
+
+  // archive_data_load
   write_u32((u32 *)0x0200750a, 0xff79f3bc);
+
+  // archive_data_load_malloc
+  write_u32_16bit_alligned((u16 *)0x02007526, 0xf96bf3bd);
+
+  // load_narc: bl 0x023c5000, loadSpriteFromNarc
+  // write_u32_16bit_alligned((u16 *)0x02009542, 0xfd5df3bb);
+
+  // inject_sprite: bl 0x023c5100, loadOrInjectSprite
+  write_u32_16bit_alligned((u16 *)0x0200956A, 0xfdc9f3bb);
+
+  // inject_palette: bl 0x023c5200, loadOrInjectPalette
+  write_u32((u32 *)0x0200999c, 0xfc30f3bb);
+
   __asm__ volatile("pop {r0-r7, pc}\n");
 }
