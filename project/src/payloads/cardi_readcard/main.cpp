072892acbc8c05559ea0d131e4e9b76742b9f138
DevreeseJorik <jorik.devreese@outlook.com>
2025-12-29 10:28:21 +0100

    cardi_readcard: remove helpers
    Signed-off-by: DevreeseJorik <jorik.devreese@outlook.com>

--- project/src/payloads/cardi_readcard/main.cpp
+++ project/src/payloads/cardi_readcard/main.cpp
@@ -1,24 +1,6 @@
 #include "libs/util/functions.h"
-#include "libs/util/memory.h"
 #include "libs/util/hooks.hpp"
-
-void write_if_matching8(u8 *address, u32 match, u8 value) {
-  if (read_u32_unaligned((u32 *)address) == match) {
-    write_u8(address, value);
-  }
-}
-
-void write_if_matching16(u16 *address, u32 match, u16 value) {
-  if (read_u32_unaligned((u32 *)address) == match) {
-    write_u16(address, value);
-  }
-}
-
-void write_if_matching32(u32 *address, u32 match, u32 value) {
-  if (read_u32_unaligned((u32 *)address) == match) {
-    write_u32(address, value);
-  }
-}
+#include "libs/util/memory.h"
 
 __attribute__((naked)) __attribute__((section(".text.main")))
 __attribute__((target("arm"))) void
@@ -29,7 +11,8 @@ main_entry(void) {
   // custom overlay modifications:
 
   // disable sprite pop up in battle
-  write_if_matching8((u8 *)0x0221fde4, 0xDA002804, 0x0);
+  if (*(u32 *)0x0221fde4 == 0xDA002804)
+    write_u8((u8 *)0x0221fde4, 0x0);
 
   __asm__ volatile("pop {r0-r7, pc}\n");
 }
