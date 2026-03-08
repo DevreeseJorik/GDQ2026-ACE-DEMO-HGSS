fbafadd22a7d2b66a050f6b9a9714677fcd3d1c8
DevreeseJorik <jorik.devreese@outlook.com>
2026-03-08 08:58:46 +0100

    MIi_UncompressBackward: add hook
    This function is called very early on after boot and re-reloads ROM to RAM again.
    This wasn't the case in dp during previous tests.
    
    This hook is necessary to then again retain code execution by one-off statically compiled functions.
    e.g: the NARC load function to allow dynamic overlays to be hijacked.
    
    Signed-off-by: DevreeseJorik <jorik.devreese@outlook.com>

---
+++ project/src/payloads/MIi_UncompressBackward/main.c
@@ -0,0 +1,9 @@
+#include "util/memory.h"
+
+__attribute__((naked)) __attribute__((section(".text.main")))
+__attribute__((target("arm"))) void
+main(void) {
+  __asm__ volatile("push {r0-r7, lr}\n");
+  write_u32((u32 *)0x0020dd8c8, 0x23C4100);
+  __asm__ volatile("pop {r0-r7, pc}\n");
+}
