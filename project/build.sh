97e10bb3394623883856bd4b8c5ea7d3e7a3a2ce
DevreeseJorik <jorik.devreese@outlook.com>
2026-03-08 09:53:45 +0100

    build.sh, packager: add build script
    Signed-off-by: DevreeseJorik <jorik.devreese@outlook.com>

---
+++ project/build.sh
@@ -0,0 +1,15 @@
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake ../
+make
+
+cd ..
+
+image_converter.py -i=assets -o=/home/project/output/images/bins
+
+mkdir /home/project/output/packages
+packager.py -i=/home/project/output/payloads -o=/home/project/output/packages/bins/
+
+qhexdump /home/project/output/packages/bins/packed.bin > /home/project/output/packages/bins/packed_dump.txt
+qhexdump /home/project/output/packages/bins/unpacked.bin > /home/project/output/packages/bins/unpacked_dump.txt
